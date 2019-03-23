29a9164fe75e7889a3f5c60a1647efa6fb47cd8b
empitsu <yoshie8080@gmail.com>
2019-03-23 16:47:59 +0900

    Finally it has accepted!

--- ABC085C/Main.js
+++ ABC085C/Main.js
@@ -4,18 +4,25 @@ function Main(input) {
   const N = parseInt(tmp[0], 10);
   const Y = parseInt(tmp[1], 10);
   let ans = [];
- 
-  for (let i = 0; i <= N; i++) {
-    for (let j = 0; j <= N - i; j++) {
-      for (let k = 0; k <= N - i - j; k++) {
-        if (i * 10000 + j * 5000 + k * 1000 === Y && i + j + k === N)
-          ans.push([i, j, k]);
-      }
+
+  var a = Math.floor(Y / 10000);
+  a = a > N ? N : a;
+
+  for (let i = 0; i <= a; i++) {
+    var aRemainder = Y - 10000 * i;
+    var b = Math.floor(aRemainder / 5000);
+    b = b > N - i ? N - i : b;
+    for (let j = 0; j <= b; j++) {
+      var bRemainder = aRemainder - 5000 * j;
+      var k = Math.floor(bRemainder / 1000);
+
+      if (i * 10000 + j * 5000 + k * 1000 === Y && i + j + k === N)
+        ans.push([i, j, k]);
     }
   }
- 
   if (!ans.length) ans.push([-1, -1, -1]);
   console.log(ans[0].join(" "));
 }
- 
+
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+
