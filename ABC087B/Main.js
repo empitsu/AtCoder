912ac49b4bcd725b05b675d3caf1f1847b06be93
empitsu <yoshie8080@gmail.com>
2019-03-23 17:24:57 +0900

    Tweaked code.

--- ABC087B/Main.js
+++ ABC087B/Main.js
@@ -1,28 +1,28 @@
 function Main(input) {
+  "use strict";
   const A_PRICE = 500;
   const B_PRICE = 100;
   const C_PRICE = 50;
-  var tmp = input.split("\n");
-  var X = parseInt(tmp[3], 10);
+  const tmp = input.split("\n");
+  const X = parseInt(tmp[3], 10);
  
-  var A_MAX = parseInt(tmp[0], 10);
-  var B_MAX = parseInt(tmp[1], 10);
-  var C_MAX = parseInt(tmp[2], 10);
-  var patternAry = [];
-
-  var a = Math.floor(X / A_PRICE);
-  a = a > A_MAX ? A_MAX : a;
+  const A_MAX = parseInt(tmp[0], 10);
+  const B_MAX = parseInt(tmp[1], 10);
+  const C_MAX = parseInt(tmp[2], 10);
+  const patternAry = [];
   
-  for (var i = a; i >= 0; i--) {
-    var aRemainder = X - A_PRICE * i;
-    var b = Math.floor(aRemainder / B_PRICE);
+  let a = Math.floor(X / A_PRICE);
+  a = a > A_MAX ? A_MAX : a;
+ 
+  for (let i = 0; i <= a; i++) {
+    let b = Math.floor((X - A_PRICE * i) / B_PRICE);
     b = b > B_MAX ? B_MAX : b;
-    for (var j = b; j >= 0; j--) {
-      var bRemainder = aRemainder - B_PRICE * j;
-      var c = Math.floor(bRemainder / C_PRICE);
+    for (let j = 0; j <=b; j++) {
+      let c = Math.floor((X - A_PRICE * i - B_PRICE * j) / C_PRICE);
       c = c > C_MAX ? C_MAX : c;
       if (i * A_PRICE + j * B_PRICE + c * C_PRICE === X) {
         patternAry.push({ a: i, b: j, c: c });
+       
       }
     }
   }
