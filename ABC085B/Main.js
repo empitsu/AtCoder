58149f73ce94298c9049da7a5b65b5164bf3dad0
empitsu <yoshie8080@gmail.com>
2019-04-04 01:56:21 +0900

    I had to take a blank line of the end into account.

--- ABC085B/Main.js
+++ ABC085B/Main.js
@@ -2,15 +2,7 @@ function Main(input) {
   "use strict";
   const tmp = input.split("\n");
   const dAry = tmp.slice(1);
-  const kagamiMochiAry = [];
-  dAry.sort((a, b) => {
-    return parseInt(b, 10) - parseInt(a, 10);
-  });
-  kagamiMochiAry.push(dAry[0]);
- 
-  for (let i = 0; i < dAry.length - 1; i++) {
-    if (dAry[i] !== dAry[i + 1]) kagamiMochiAry.push(dAry[i + 1]);
-  }
+  const kagamiMochiAry = dAry.filter((val,i,self)=> val && self.indexOf(val) ===i);
   console.log(kagamiMochiAry.length);
 }
  
