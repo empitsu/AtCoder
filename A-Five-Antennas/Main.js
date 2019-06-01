69daa41724f84ab05765d4bfd8a7f1a0102f105d
empitsu <yoshie8080@gmail.com>
2019-06-01 22:49:09 +0900

    Refactoring.

--- A-Five-Antennas/Main.js
+++ A-Five-Antennas/Main.js
@@ -1,22 +1,10 @@
 function Main(input) {
   var tmp = input.trim().split("\n");
   var array = tmp.slice(0, tmp.length - 1);
-  var max = parseInt(tmp[tmp.length - 1], 10);
+  var limit = parseInt(tmp[tmp.length - 1], 10);
   array = array.map(val => parseInt(val, 10));
-  var success = true;
 
-  for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length; j++) {
-      if (i >= j) continue;
-      var dist = array[j] - array[i];
-
-      if (max < dist) {
-        success = false;
-        break;
-      }
-    }
-  }
-  console.log(success ? "Yay!" : ":(");
+  console.log(array[array.length - 1] - array[0] <= limit ? "Yay!" : ":(");
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
