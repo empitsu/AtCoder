247649a2886bd62600186a5b127fa8637b77fc2f
empitsu <yoshie8080@gmail.com>
2019-06-02 10:22:08 +0900

    Simplified code in order to reduce number of iterations.

--- B-Five-Dishes/Main.js
+++ B-Five-Dishes/Main.js
@@ -1,24 +1,16 @@
 function Main(input) {
   var array = input.trim().split("\n");
   array = array.map(val => parseInt(val, 10));
-  var waitTimeAry = [];
   var amount = 0;
-  var ceilArray = [];
- 
+  var longestWaitTime = 0;
+
   for (var i = 0; i < array.length; i++) {
     var ceil = Math.ceil(array[i] / 10) * 10;
-    waitTimeAry.push(ceil - array[i]);
-    ceilArray.push(ceil);
+    amount += ceil;
+    longestWaitTime =
+      ceil - array[i] > longestWaitTime ? ceil - array[i] : longestWaitTime;
   }
- 
-  var maxWait = Math.max(...waitTimeAry);
-  var index = waitTimeAry.indexOf(maxWait);
-  ceilArray.splice(index, 1);
-  ceilArray.splice(ceilArray.length, 0, array[index]);
-  for (var j = 0; j < ceilArray.length; j++) {
-    amount += ceilArray[j];
-  }
-  console.log(amount);
+  console.log(amount - longestWaitTime);
 }
- 
+
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
